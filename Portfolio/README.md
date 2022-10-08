5593fba9ce3a768e848946745c4bb83b1e00fbbd
samarth-kumar-05 <samarthkumar051102@gmail.com>
2022-10-08 13:57:14 +0530

    added a portfolio website

---
+++ Portfolio/README.md
@@ -0,0 +1,13 @@
+
+# Tic-Tac-Toe
+This is a  portfolio website developed in HTML, CSS and JavaScript.
+The website was made by me for Frontend practice in my initial phase with the help of tutorials.
+
+It contains all the sections which should be present in a portfolio website.
+however it is just a landing page and users can add their projects, social accounts themselves.
+## Snapshot
+![portfolio](https://user-images.githubusercontent.com/97377330/194697825-97e21609-1f51-486e-a995-95456a39911c.PNG)
+
+
+
+# [![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/built-by-developers.svg)](https://forthebadge.com)
---
+++ Portfolio/script.js
@@ -0,0 +1,55 @@
+var progressBars = document.querySelectorAll(".skill-progress > div");
+
+
+
+function initialiseBar(bar) {
+    bar.setAttribute("data-visited", false);
+    bar.style.width = 0 + '%';
+}
+
+for (var bar of progressBars) {
+    initialiseBar(bar);
+}
+
+
+
+function fillBar(bar) {
+
+    var currentWidth = 0;
+    var targetWidth = bar.getAttribute("data-bar-width");
+    var interval = setInterval(function () {
+        if (currentWidth >= targetWidth) {
+            clearInterval(interval);
+            return;
+        }
+        currentWidth++;
+        bar.style.width = currentWidth + '%';
+    }, 5);
+
+}
+
+
+
+// This function uses a for loop for individual progress bars.
+function checkScroll() {
+
+    for (let bar of progressBars) {
+        var barCoordinates = bar.getBoundingClientRect();
+        if ((bar.getAttribute("data-visited") == "false") &&
+            (barCoordinates.top <= (window.innerHeight - barCoordinates.height))) {
+            bar.setAttribute("data-visited", true);
+            fillBar(bar);
+        } else if (barCoordinates.top > window.innerHeight) {
+            bar.setAttribute("data-visited", false);
+            initialiseBar(bar);
+        }
+
+    }
+}
+
+
+
+window.addEventListener("scroll", checkScroll);
+
+// This event fills the progress bars if they are displayed on the screen when the page is loaded.
+//window.addEventListener("load", checkScroll);
