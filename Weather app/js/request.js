33a064e7e1606d9c6dc7f9984380ba80305c48fd
AMIT KUMAR BERA <84179065+itzwow@users.noreply.github.com>
2022-10-10 00:34:05 +0530

    Weather app added

---
+++ Weather app/js/request.js
@@ -0,0 +1,14 @@
+const key = 'cbe3dd267a18f6c89943b3eff94f1ed7';
+
+const requestCity = async (city) => {
+    const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
+    const query = `?q=${city}&appid=${key}`;
+
+    //make fetch call (promise call)
+    const response = await fetch(baseURL + query);
+
+    //promise data
+    const data = await response.json();
+    return data;
+
+}
