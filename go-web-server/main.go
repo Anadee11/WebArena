17df63bcc966fec6aa7f3b31a14a5885902efaf7
Jayesh Mann <jayesh.mann@gojek.com>
2022-10-09 19:13:37 +0530

    feat: hello endpoint with error checking

--- go-web-server/main.go
+++ go-web-server/main.go
@@ -6,9 +6,23 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintf(w, "Hello from /hello endpoint!")
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
